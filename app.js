15686a03e6e075888b776f35790183618e5e2bbf
ParveenDavid2394 <parveendavid2394@gmail.com>
2020-09-11 17:38:32 +0800

    added toggle feature

--- app.js
+++ app.js
@@ -3,3 +3,16 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', () => {
+    // if .show-link not in classList, then add else remove
+    // links.classList.contains('show-links') ? 
+    // links.classList.remove('show-links') : links.classList.add('show-links');
+
+    // can do the same thing in one line
+    links.classList.toggle('show-links');
+})
